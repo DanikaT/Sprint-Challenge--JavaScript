4871d6e006b8de89fb5605f1a6fcd0aabb49510e
Danika Thomson <danikathomson@macusers-iMac.lan>
2019-08-23 09:41:52 +0100

    Finished university array questions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -70,11 +70,23 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
-
+/* Request 1: Create a new array called universities that contains all the universities in the graduates array.  z
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for (let i = 0; i < graduates.length; i++) {
+  universities.push(graduates[i].university);
+  
+}
 console.log(universities)
+
+let universitiesSorted = function(){
+return universities.sort();
+};
+
+
+console.log(universitiesSorted());
+
+
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
