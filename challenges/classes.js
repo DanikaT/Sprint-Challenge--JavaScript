ee3875b8736223ec8b65941bb0854f1b86436674
Danika Thomson <danikathomson@macusers-iMac.local>
2019-10-19 10:09:02 +0200

    refactored protypes to class syntax

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,35 +1,32 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
-/*function CuboidMaker(cuboidMakerAttributes) {
-  this.length = cuboidMakerAttributes.length;
-  this.width = cuboidMakerAttributes.width;
-  this.height = cuboidMakerAttributes.height;
+class CuboidMaker{
+  constructor(attributes){
+  this.length = attributes.length;
+  this.width = attributes.width;
+  this.height = attributes.height;
 }
-*/
+volume() {
+  return this.length * this.width * this.height;
+ }
 
-
-class LambdaStudent {
-    constructor(lambdastudent.Attributes){
-        this.lovesJavascript = lambdastudent.lovesJavascript;
-        this.name = lambdastudent.name;
-        this.passExam = lambdastudent.passExam;
-    }
-
-
-    passexam() {
-        return ` Hello! My name is ${this.name}. I really love JS. Today I passed my ${this.passexam} `
-    }
-
+surfaceArea() {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
 }
-const webeu3 = new LambdaStudent({
-    lovesJavascript: true,
-    name: "Danika",
-    passExam: true,
-  })
+}
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+})
+
+
+
+
 
 
 
 //Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(webeu3.passExam()); // 100
-//console.log(cuboid.surfaceArea()); // 130
+ // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
